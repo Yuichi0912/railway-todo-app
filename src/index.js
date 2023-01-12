8e6bb9b64fc982d4321acd873e840d4ec1fa5b79
Yuichi0912 <icec0912@gmail.com>
2023-01-12 23:39:13 +0900

    完成版

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <CookiesProvider>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </CookiesProvider>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <CookiesProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </CookiesProvider>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Browserrouterのラップを足した。
